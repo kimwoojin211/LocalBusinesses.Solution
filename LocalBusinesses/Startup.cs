3e31ddc15efeda8a697440df4d0651a74b655ef9
Woo Jin Kim <kimwoojin211@gmail.com>
2021-04-02 15:09:34 -0700

    add swagger documentation

--- LocalBusinesses/Startup.cs
+++ LocalBusinesses/Startup.cs
@@ -1,9 +1,11 @@
+using System;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 using LocalBusinesses.Models;
 
 namespace LocalBusinesses
@@ -24,7 +26,26 @@ namespace LocalBusinesses
       services.AddDbContext<LocalBusinessesContext>(opt =>
           opt.UseMySql(Configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(Configuration["ConnectionStrings:DefaultConnection"])));
       services.AddControllers();
-
+      services.AddSwaggerGen(c =>
+      {
+          c.SwaggerDoc("v1", new OpenApiInfo
+          {
+              Version = "v1",
+              Title = "LocalBusinesses API",
+              Description = "ASP.NET Core Web API that holds information on local businesses in the area",
+              Contact = new OpenApiContact
+              {
+                  Name = "Woo Jin Kim",
+                  Email = "kimwoojin211@gmail.com",
+                  Url = new Uri("https://github.com/kimwoojin211"),
+              },
+              License = new OpenApiLicense
+              {
+                  Name = "Use under MIT",
+                  Url = new Uri("https://https://opensource.org/licenses/MIT"),
+              }
+          });
+      });
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,7 +61,11 @@ namespace LocalBusinesses
         // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
         app.UseHsts();
       }
-
+      app.UseSwagger();
+      app.UseSwaggerUI(c =>
+      {
+          c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+      });
 
       app.UseRouting();
 
